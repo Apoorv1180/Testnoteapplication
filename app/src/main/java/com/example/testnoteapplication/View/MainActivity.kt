8491bb71412968038c54c413263feef16531919f
Jyoti-Nalwade <jyoti.nalwade5@gmail.com>
2020-04-27 20:36:15 -0700

    Initial commit

---
+++ app/src/main/java/com/example/testnoteapplication/View/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testnoteapplication.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testnoteapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
