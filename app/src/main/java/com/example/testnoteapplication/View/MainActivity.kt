9806a7a3ee6757e479385e7af9843aa191315920
Jyoti-Nalwade <jyoti.nalwade5@gmail.com>
2020-04-28 03:02:06 -0700

    ui updates

--- app/src/main/java/com/example/testnoteapplication/View/MainActivity.kt
+++ app/src/main/java/com/example/testnoteapplication/View/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.testnoteapplication.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.testnoteapplication.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_nav )
     }
 }
