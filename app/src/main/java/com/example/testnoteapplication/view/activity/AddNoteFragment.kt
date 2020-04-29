865edd5aedda200f572fa1b44a16aee39f7ff137
apoorv1180 <apoorv111221cse>
2020-04-29 18:04:29 +0530

    SamplePush

--- app/src/main/java/com/example/testnoteapplication/view/activity/AddNoteFragment.kt
+++ app/src/main/java/com/example/testnoteapplication/view/activity/AddNoteFragment.kt
@@ -1,11 +1,13 @@
 package com.example.testnoteapplication.view.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.testnoteapplication.R
 import com.example.testnoteapplication.data.model.NotesModel
@@ -29,13 +31,20 @@ class AddNoteFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(AddNoteViewModel::class.java)
         // TODO: Use the ViewModel
-        savenote.setOnClickListener{
+//        savenote.setOnClickListener{
             var notesModel = NotesModel(1, R.id.noteTitle.toString(), R.id.noteDescription.toString())
-            val isNoteAdded = viewModel.addNote(notesModel)
-            if(isNoteAdded) {
+//            val isNoteAdded = viewModel.addNote(notesModel)
+//            if(isNoteAdded) {
+//                Toast.makeText(this.context,"Success", Toast.LENGTH_LONG).show() //.maketext(this,"Success", Toast.LENGTH_LONG)
+//            }
+//        }
+//        viewModel.addNote(notesModel).observe(this, Observer<List<CountryModel>> { countryList ->
+//            Log.e(MainActivity::class.java.simpleName,countryList.toString())
+        viewModel.addNote(notesModel).observe(this,Observer<Boolean>){ value ->
+            Log.e("TAG",String.valueOf(value));
+            if(value) {
                 Toast.makeText(this.context,"Success", Toast.LENGTH_LONG).show() //.maketext(this,"Success", Toast.LENGTH_LONG)
-            }
-        }
+           }
     }
 
 
