eff959c957d95da70670dc5c08c1a3464e1f0bef
Jyoti-Nalwade <jyoti.nalwade5@gmail.com>
2020-05-11 23:51:34 -0700

    edit subscription

--- app/src/main/java/com/example/testnoteapplication/view/fragment/EditSubscriptionFragment.kt
+++ app/src/main/java/com/example/testnoteapplication/view/fragment/EditSubscriptionFragment.kt
@@ -3,6 +3,7 @@ package com.example.testnoteapplication.view.fragment
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,48 +11,63 @@ import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProviders
+
 import com.example.testnoteapplication.R
 import com.example.testnoteapplication.Util.NoteUtil
 import com.example.testnoteapplication.data.model.AllNotesModel
 import com.example.testnoteapplication.view.adapter.CustomAdapterSpinnerSub
 import com.example.testnoteapplication.viewmodel.AddSubscriptionViewModel
+import com.example.testnoteapplication.viewmodel.EditSubscriptionViewModel
 import kotlinx.android.synthetic.main.add_subscription_fragment.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class AddSubscriptionFragment : DialogFragment() {
+class EditSubscriptionFragment : DialogFragment() {
 
-    companion object {    }
+    companion object {
+        fun newInstance() = EditSubscriptionFragment()
+    }
     //Var declaration
-    private lateinit var viewModel: AddSubscriptionViewModel
+    private lateinit var viewModel: EditSubscriptionViewModel
     lateinit var dateTextView: TextView
     var cal = Calendar.getInstance()
     lateinit var subName : String
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setUpView(view)
-        initViewModel()
-        initListner()
-        observeAddSubscriptionViewModel()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.edit_subscription_fragment, container, false)
     }
 
-    private fun observeAddSubscriptionViewModel() {
-        viewModel.getValue().observe(viewLifecycleOwner,Observer<Boolean>{ value ->
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(EditSubscriptionViewModel::class.java)
+
+    }
+
+     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpView(view)
+        initViewModel()
+        initListener()
+        observeEditSubscriptionViewModel()
+    }
+    private fun observeEditSubscriptionViewModel() {
+        //todo temp : wirte seter and complete
+        /*viewModel.getValue().observe(viewLifecycleOwner,Observer<Boolean>{ value ->
             if(value){
                 Toast.makeText(context, "Added to Database", Toast.LENGTH_LONG).show()
                 closeCurrentFragment()
             }else
                 Log.e("NO ","No");
-        })
+        })*/
+    }
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(EditSubscriptionViewModel::class.java)
     }
 
-    private fun initViewModel() {
-        viewModel = ViewModelProviders.of(this).get(AddSubscriptionViewModel::class.java)
-    }
-    private fun initListner() {
+    private fun initListener() {
         expiryDate.setOnClickListener { v ->
             when (v?.id) {
                 R.id.expiryDate -> openDatePicker(v)
@@ -70,10 +86,10 @@ class AddSubscriptionFragment : DialogFragment() {
                         subDescription.text.toString(),
                         NoteUtil.SUB,"",
                         dateTextView.text.toString(),2)
-        viewModel.addSubscriptionVm(notesModel)
+        //update task call
+        //viewModel.addSubscriptionVm(notesModel)
 
     }
-
     private fun closeCurrentFragment() {
         fragmentManager?.beginTransaction()?.remove(this)?.commit()
     }
@@ -93,45 +109,38 @@ class AddSubscriptionFragment : DialogFragment() {
                 subName=subscriptions_array[position]
                 if(subscriptions_array[position]=="Add Custom"){
                     Toast.makeText(
-                        view.context,
-                        "Other subsription " ,
-                        Toast.LENGTH_SHORT
+                            view.context,
+                            "Other subsription " ,
+                            Toast.LENGTH_SHORT
                     ).show()
                 }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
                 Toast.makeText(
-                    view.context,
-                    "Please select subscription name  ",
-                    Toast.LENGTH_SHORT
+                        view.context,
+                        "Please select subscription name  ",
+                        Toast.LENGTH_SHORT
                 ).show()
             }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.add_subscription_fragment, container, false)
-    }
     private fun openDatePicker(v: View?) {
 
         val dateSetListener =
-            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, monthOfYear)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                updateDateInView()
-            }
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    cal.set(Calendar.YEAR, year)
+                    cal.set(Calendar.MONTH, monthOfYear)
+                    cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    updateDateInView()
+                }
         DatePickerDialog(
-            v?.context!!,
-            dateSetListener,
-            cal.get(Calendar.YEAR),
-            cal.get(Calendar.MONTH),
-            cal.get(Calendar.DAY_OF_MONTH)
+                v?.context!!,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
         ).show()
     }
     private fun updateDateInView() {
@@ -140,5 +149,6 @@ class AddSubscriptionFragment : DialogFragment() {
         val format = sdf.format(cal.time)
         dateTextView.text = format
     }
+
 
 }
---
+++ app/src/main/java/com/example/testnoteapplication/viewmodel/EditSubscriptionViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.testnoteapplication.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+
+class EditSubscriptionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
